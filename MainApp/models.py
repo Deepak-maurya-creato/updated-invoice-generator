b05d7c0d86f824fe10226dff688aabeade8166bf
Deepak Maurya <ajmaurya119@gmail.com>
2024-03-18 17:19:24 +0530

    Add Client Module about to complite

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class ClientServiceProvider(models.Model):
+    # Client details
+    client_comp_name = models.CharField(max_length=120)
+    client_gst = models.CharField(max_length=50)
+    client_phone = models.CharField(max_length=20)
+    client_email = models.EmailField()
+    client_country = models.CharField(max_length=50)
+    client_state = models.CharField(max_length=50)
+    client_pin = models.CharField(max_length=20)
+    client_other_info = models.TextField()
+
+    # Service provider details
+    provider_comp_name = models.CharField(max_length=120)
+    provider_name = models.CharField(max_length=100)
+    provider_acc_no = models.CharField(max_length=100)
+    provider_bank_name = models.CharField(max_length=100)
+    provider_ifsc = models.CharField(max_length=50)
+    provider_gst = models.CharField(max_length=50)
+    provider_phone = models.CharField(max_length=20)
+    provider_mail = models.EmailField()
+    provider_other_info = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"Client Name/Company: {self.client_comp_name} - Provider: {self.provider_comp_name} - created_at: {self.created_at}"
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect,HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-
+from django.contrib import messages
+from .models import *
 
 # Create your views here.
 
@@ -29,5 +30,53 @@ def home(request):
 
 @login_required
 def add_client(request):
+    if request.method == 'POST':
+        try:
+            # Extracting data from the request
+            client_comp_name = request.POST.get('client_comp_name')
+            client_gst = request.POST.get('client_gst')
+            client_phone = request.POST.get('client_phone')
+            client_email = request.POST.get('client_email')
+            client_country = request.POST.get('client_country')
+            client_state = request.POST.get('client_state')
+            client_pin = request.POST.get('client_pin')
+            client_other_info = request.POST.get('client_other_info')
+
+            provider_comp_name = request.POST.get('provider_comp_name')
+            provider_name = request.POST.get('provider_name')
+            provider_acc_no = request.POST.get('provider_acc_no')
+            provider_bank_name = request.POST.get('provider_bank_name')
+            provider_ifsc = request.POST.get('provider_ifsc')
+            provider_gst = request.POST.get('provider_gst')
+            provider_phone = request.POST.get('provider_phone')
+            provider_mail = request.POST.get('provider_mail')
+            provider_other_info  = request.POST.get('provider_other_info')
+
+            # Creating and saving the model object
+            client_service_provider = ClientServiceProvider(
+                client_comp_name=client_comp_name,
+                client_gst=client_gst,
+                client_phone=client_phone,
+                client_email=client_email,
+                client_country=client_country,
+                client_state=client_state,
+                client_pin=client_pin,
+                client_other_info=client_other_info,
+                provider_comp_name=provider_comp_name,
+                provider_name=provider_name,
+                provider_acc_no=provider_acc_no,
+                provider_bank_name=provider_bank_name,
+                provider_ifsc=provider_ifsc,
+                provider_gst=provider_gst,
+                provider_phone=provider_phone,
+                provider_mail=provider_mail,
+                provider_other_info=provider_other_info
+            )
+            client_service_provider.save()
+
+            messages.success(request, 'Data saved successfully.')
+
+        except Exception as e:
+            messages.error(request, f'Error occurred: {str(e)}')
 
     return render(request, 'add_client.html')
